2ef73a7b36ba74371379dd68bf6c52c45e6fdb66
susie <zhongqi.liu@baozun.com>
2018-10-26 17:15:50 +0800

    factory pattern start

---
+++ factoryPattern/src/main/java/com/naka/demo/factoryPattern/App.java
@@ -0,0 +1,13 @@
+package com.naka.demo.factoryPattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
