906cd12720d34760ab901792d1e3a2ae38a88ad8
susie <zhongqi.liu@baozun.com>
2019-05-23 22:33:32 +0800

    test

--- src/main/java/com/example/demo/ServletInitializer.java
+++ src/main/java/com/example/demo/ServletInitializer.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
-
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+/**
+ * @description:
+ * @author: susie
+ * @create: 2019-05-23 22:31
+ **/
+public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //此处的Application.class为带有@SpringBootApplication注解的启动类
         return builder.sources(DemoApplication.class);
     }
+
 }
